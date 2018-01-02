52178b61e9b291276dee19665e9fab215d467059
YChiHo <chiho2736@gmail.com>
2018-01-02 18:45:02 +0900

    payload
    trash value..

--- http_parser.h
+++ http_parser.h
@@ -5,11 +5,16 @@
 #include <fstream>
 #include <string>
 #include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
 #include <pcap/pcap.h>
+#include <arpa/inet.h>
 //#include <direct.h>
 #endif // HTTP_PARSER_H
 
 #define PCAP_OPENFLAG_PROMISCUOUS 1
+#define TYPE_IP 0x0800
+#define TYPE_TCP 0x06
 
 #ifdef _WIN32_
 #define WPCAP
@@ -17,76 +22,86 @@
 #endif
 using namespace std;
 
-typedef struct eth_hdr {	//ethernet_header
-	unsigned char eth_dest[6];
-	unsigned char eth_src[6];
-	u_short eth_type;
-} ETH_HDR, *ETH_HDR;
+typedef struct ip_address{
+    u_char byte1;
+    u_char byte2;
+    u_char byte3;
+    u_char byte4;
+}ip_address;
 
-typedef struct ip_hdr {		//ip_header
-	u_int8_t  HdrLength:4;
-	u_int8_t  Version:4;
-	u_int8_t  TOS;
-	u_int16_t Length;
-	u_int16_t Id;
-	u_int16_t FragOff0;
-	u_int8_t  TTL;
-	u_int8_t  Protocol;
-	u_int16_t Checksum;
-	u_int32_t SrcAddr;
-	u_int32_t DstAddr;
-} IP_HDR, *IP_HDR;
+typedef struct eth_hdr {  //ethernet_header
+  unsigned char eth_dest[6];
+  unsigned char eth_src[6];
+  u_short eth_type;
+} ETH_HDR;
 
-typedef struct tcp_hdr
-{
-	u_int16_t SrcPort;
-	u_int16_t DstPort;
-	u_int32_t SeqNum;
-	u_int32_t AckNum;
-	u_int16_t Reserved1:4;
-	u_int16_t HdrLength:4;
-	u_int16_t Fin:1;
-	u_int16_t Syn:1;
-	u_int16_t Rst:1;
-	u_int16_t Psh:1;
-	u_int16_t Ack:1;
-	u_int16_t Urg:1;
-	u_int16_t Reserved2:2;
-	u_int16_t Window;
-	u_int16_t Checksum;
-	u_int16_t UrgPtr;
-} TCP_HDR, *TCP_HDR;
+typedef struct ip_hdr {   //ip_header
+  u_int8_t  HdrLength:4;
+  u_int8_t  Version:4;
+  u_int8_t  TOS;
+  u_int16_t Length;
+  u_int16_t Id;
+  u_int16_t FragOff0;
+  u_int8_t  TTL;
+  u_int8_t  Protocol;
+  u_int16_t Checksum;
+  ip_address SrcAddr;
+  ip_address DstAddr;
+} IP_HDR;
+
+typedef struct tcp_hdr{   //tcp_header
+  u_int16_t SrcPort;
+  u_int16_t DstPort;
+  u_int32_t SeqNum;
+  u_int32_t AckNum;
+  u_int16_t Reserved1:4;
+  u_int16_t HdrLength:4;
+  u_int16_t Fin:1;
+  u_int16_t Syn:1;
+  u_int16_t Rst:1;
+  u_int16_t Psh:1;
+  u_int16_t Ack:1;
+  u_int16_t Urg:1;
+  u_int16_t Reserved2:2;
+  u_int16_t Window;
+  u_int16_t Checksum;
+  u_int16_t UrgPtr;
+} TCP_HDR;
 
 class Http_Parser {
 
 public:
 
-	typedef struct _line {
-		string one;
-		string two;
-		string three;
-	}Line;
+  typedef struct _line {
+    string one = "";
+    string two;
+    string three;
+  }Line;
 
-	typedef struct _message {
-		Line *first_Line;
-		string header;
-		string body;
-	}message;
-
-public:
-	Http_Parser();  //init
-	void init(message *msg);
-	string Get_Data(string filepath);
-	string Get_Line(string data);
-	void pcap_init();
-	void Reqeust_Line_Parser(string requestLine);
-	bool request_Option(string method);
-	void message_Parser();
-	void err_print();
+  typedef struct _message {
+    Line *first_Line;
+    string header;
+    string body;
+  }message;
 
 public:
-	ETH_HDR *ethhdr = new ETH_HDR;
-	IP_HDR *iphdr = new IP_HDR;
-	TCP_HDR *tcpphdr = new TCP_HDR;
+  Http_Parser();  //init
+  void init(message *msg);
+  string Get_Data(string filepath);
+  string Get_Line(string data);
+  void pcap_run();
+  void task(pcap_t *handle, struct pcap_pkthdr *header, u_char *packet);
+  void Reqeust_Line_Parser(string requestLine);
+  bool request_Option(string method);
+  void message_Parser();
+  void err_print();
+
+public:
+  ETH_HDR *ethhdr = new ETH_HDR;
+  IP_HDR *iphdr = new IP_HDR;
+  TCP_HDR *tcphdr = new TCP_HDR;
 
 };
+
+
+
--- main.cpp
+++ main.cpp
@@ -3,14 +3,14 @@
 Http_Parser::Http_Parser() {
 }
 
-// 변수 초기화
+// init variable
 void Http_Parser::init(message *msg) {
 	try {
-		msg->first_Line->one.clear();
-		msg->first_Line->two.clear();
-		msg->first_Line->three.clear();
-		msg->header.clear();
-		msg->body.clear();
+		// msg->first_Line->one.clear();
+		// msg->first_Line->two.clear();
+		// msg->first_Line->three.clear();
+		// msg->header.clear();
+		// msg->body.clear();
 	}
 	catch (int Exception) {
 		err_print();
@@ -18,7 +18,7 @@ void Http_Parser::init(message *msg) {
 	}
 }
 
-// 파일에서 데이터 읽기
+// read data
 string Http_Parser::Get_Data(string filepath) {
 
 	string read_Data = NULL;
@@ -48,34 +48,65 @@ string Http_Parser::Get_Line(string data) {
 	return line;
 }
 
-void Http_Parser::pcap_init() {
+void Http_Parser::pcap_run() {
 	pcap_t *handle;																	/* Session handle				 */
-	struct pcap_pkthdr *header;														/*								 */
-	char errbuf[PCAP_ERRBUF_SIZE];													/* Error string					 */
-	bpf_u_int32 mask = 0;															/* Our netmask					 */
-	bpf_u_int32 net = 0;															/* Our IP						 */
-	u_char *packet;																	/*				  				 */
-	pcap_if_t *alldevs, *d;															/*	 							 */
-	int i = 0, num;
+	u_char *packet;
+	struct pcap_pkthdr *header;
+    char errbuf[PCAP_ERRBUF_SIZE];
 
-	if ((handle = pcap_open_live(d->name, BUFSIZ, PCAP_OPENFLAG_PROMISCUOUS, 1, errbuf)) == NULL) {	//장치이름, 패킷캡쳐부분, promiscuous mode, 시간, 에러버퍼
-		fprintf(stderr, "Couldn't open device %s: %s\n", d->name, errbuf);
+	if ((handle = pcap_open_live("eth1", BUFSIZ, PCAP_OPENFLAG_PROMISCUOUS, 10, errbuf)) == NULL) {	//device name, bufsize, promiscuous mode, time, error buffer
+		err_print();
+		pcap_close(handle);
 		return;
 	}
-
-
+	else {
+		task(handle, header, packet);
+	}
 }
 
-void task(pcap_t *handle, struct pcap_pkthdr *header, u_char *packet) {
-	int res;
-	while ((res = pcap_next_ex(handle, &header, (const u_char **)&packet)) >= 0) {
-		if (res == 0) continue;
-		else if (res == -1) {
-			cout << "pcap_next_ex Error !" << endl;
-			break;
-		}
-		else packet_hand(packet, header->caplen);
-	}
+void Http_Parser::task(pcap_t *handle, struct pcap_pkthdr *header, u_char *packet) {
+        int res;
+        u_char *payload;
+        int ip_len, size_payload;
+        while ((res = pcap_next_ex(handle, &header, (const u_char **)&packet)) >= 0) {
+                if (res == 0) continue;
+                else if (res == -1) {
+                        cout << "pcap_next_ex Error !" << endl;
+                        break;
+                }
+                else {
+
+                        ethhdr = (ETH_HDR *)packet;
+
+                        if (ntohs(ethhdr->eth_type) == TYPE_IP) {//IP
+                        		memset(&payload, 0x00, sizeof(payload));
+                                iphdr = (IP_HDR *)(sizeof(ETH_HDR) + packet);
+                                ip_len = ntohs(iphdr->Length);
+
+                                if(iphdr->Protocol == TYPE_TCP){
+                                	tcphdr = (TCP_HDR *)(sizeof(ETH_HDR) + sizeof(IP_HDR) + packet);
+                                	payload = (u_char *)(sizeof(ETH_HDR) + sizeof(IP_HDR) + sizeof(TCP_HDR) + packet + 12);
+                                	size_payload = ip_len - ( sizeof(IP_HDR) + (tcphdr->HdrLength * 4) - 9 );
+                                	payload[size_payload - 1 ] = '\0';
+                                	if(ntohs(tcphdr->SrcPort) == 80 || ntohs(tcphdr->DstPort) == 80){
+                                	/* print log */
+                                	printf("%d.%d.%d.%d:%d -> %d.%d.%d.%d:%d\n",
+                                		iphdr->SrcAddr.byte1, iphdr->SrcAddr.byte2, iphdr->SrcAddr.byte3, iphdr->SrcAddr.byte4, ntohs(tcphdr->SrcPort),
+                                		iphdr->DstAddr.byte1, iphdr->DstAddr.byte2, iphdr->DstAddr.byte3, iphdr->DstAddr.byte4, ntohs(tcphdr->DstPort));
+                                	printf("SeqNum : %u\nAckNum : %u\nReserved1 : %u\nHdrLength : %u\nReserved2 : %u\nWindow : %u\nChecksum : %u\nUrgPtr : %u\n",
+                                		tcphdr->SeqNum, tcphdr->AckNum, tcphdr->Reserved1, tcphdr->HdrLength, tcphdr->Reserved2, tcphdr->Window, tcphdr->Checksum, tcphdr->UrgPtr);
+                                	cout << "=====================================" << endl;
+                                	for(int i = 0 ; i< size_payload ; i++)
+                                		printf("%c", payload[i]);
+                                	for(int i = 0 ; i < size_payload ; i++)
+                                		printf("%02x ", payload[i]);
+                                	// cout << payload<<endl;
+                                	cout << endl << "=====================================" << endl<<endl;
+                                	}
+                                }
+                        }
+                }
+        }
 }
 
 bool Http_Parser::request_Option(string method) {
@@ -90,32 +121,36 @@ bool Http_Parser::request_Option(string method) {
 }
 
  void Http_Parser::err_print(){
- 		ofstream errfile("err.txt");					// 에러 출력할 파일 지정
- 		streambuf* orig = cerr.rdbuf();					// 기존 스트림 버퍼 저장
- 		cerr.rdbuf(errfile.rdbuf());					// 출력할 파일로 스트림버퍼 재 지정
- 		cerr.rdbuf(orig);								// 기존 스트림버퍼 복구
+ 		ofstream errfile("err.txt");					// make error file
+ 		streambuf* orig = cerr.rdbuf();					// save stream buffer
+ 		cerr.rdbuf(errfile.rdbuf());					// change stream buffer
+ 		cerr.rdbuf(orig);								// restore origin stream buffer
 		cerr.rdbuf();
  }
 
-// 실질 함수 실행
-void run(char **argv) {
+// running function
+void run(char *argv) {
 	string tmp_data = "";
 
 	Http_Parser::message request_Message;
 	Http_Parser::message response_Message;
 	Http_Parser hp;
 	hp.init(&request_Message);	hp.init(&response_Message);
-
+	hp.pcap_run();
 	//hp.Get_Line(tmp_data);
 }
 
 int main(int argc, char **argv) {
 
-	if (argc < 2 || argc > 2)
+	if (argc < 2 || argc > 2){
 		cout << "Example : " << argv[0] << " sample_file\n";
+		return 0;
+	}
 
-	while (1)
-		run(argv);
+	else{
+		while (1)
+			run(argv[1]);
+	}
 
-	return 0;
+
 }
